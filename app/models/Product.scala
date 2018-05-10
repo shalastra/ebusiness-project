6133de6d3cd9c65fb62128d2598ebc3bf1f8c737
Szymon <szymon.halastra@gmail.com>
2018-05-10 09:48:24 +0200

    Init

--- app/models/Product.scala
+++ app/models/Product.scala
@@ -5,5 +5,5 @@ import play.api.libs.json._
 case class Product(id: Long, name: String, description: String, category: Int)
 
 object Product {
-  implicit val productFormat = Json.format[Product]
+  implicit val productJSON = Json.format[Product]
 }
