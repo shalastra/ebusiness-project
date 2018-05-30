2b3ce53719eb1dc3aa973c407da8133a73752b9a
Szymon <szymon.halastra@gmail.com>
2018-05-30 20:18:25 +0200

    Remove redundant trait

--- app/models/cart/CartRepository.scala
+++ app/models/cart/CartRepository.scala
@@ -8,15 +8,15 @@ import slick.jdbc.JdbcProfile
 import scala.concurrent.Future
 
 @Singleton
-class CartRepository @Inject()(dbConfigProvider: DatabaseConfigProvider) extends CartRepositoryTrait
-  with SlickRepository with CartTable {
+class CartRepository @Inject()(dbConfigProvider: DatabaseConfigProvider)
+  extends SlickRepository with CartTable {
 
   override implicit protected final val dbConfig = dbConfigProvider.get[JdbcProfile]
 
   import dbConfig._
   import profile.api._
 
-  override def create(id: Long, orderId: Long): Future[Cart] = db.run {
+  def create(id: Long, orderId: Long): Future[Cart] = db.run {
     (cart.map(c => c.orderId)
       returning cart.map(_.id)
       into ((orderId, id) => Cart(id, orderId))
--- app/models/cart/CartRepositoryTrait.scala
+++
@@ -1,7 +0,0 @@
-package models.cart
-
-import scala.concurrent.Future
-
-trait CartRepositoryTrait {
-  def create(id: Long, orderId: Long): Future[Cart]
-}
