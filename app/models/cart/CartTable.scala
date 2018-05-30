febee94ebd1de9141c4e04788da4df5f93766bc1
Szymon <szymon.halastra@gmail.com>
2018-05-30 21:01:47 +0200

    Add missing foreign keys in db

--- app/models/cart/CartTable.scala
+++ app/models/cart/CartTable.scala
@@ -1,15 +1,17 @@
 package models.cart
 
 import models.SlickRepository
+import models.order.OrderTable
 
-trait CartTable {
-  this: SlickRepository => // has to be mixed in with SlickRepository
+trait CartTable extends OrderTable {
+  this: SlickRepository =>
 
   import driver.api._
 
   protected class CartTable(tag: Tag) extends Table[Cart](tag, "cart") {
     def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
     def orderId = column[Long]("orderId")
+    def order_fk = foreignKey("order_fk", orderId, order)(_.id)
 
     def * = (id, orderId) <> ((Cart.apply _).tupled, Cart.unapply)
   }
