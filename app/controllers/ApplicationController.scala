ab6b3c885f565726ddc92cbde8832144f1bef0c5
Szymon <szymon.halastra@gmail.com>
2018-08-26 14:32:33 +0200

    Add Silhouette

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,36 +1,51 @@
 package controllers
 
-import org.webjars.play.WebJarsUtil
-import play.api.i18n.{I18nSupport, Messages}
-import play.api.mvc.{AbstractController, AnyContent, ControllerComponents}
-import play.api.libs.json.Json.toJson
-
-import scala.concurrent.{ExecutionContext, Future}
 import javax.inject.Inject
-import play.api.Logger
-import play.api.libs.json.{JsResult, Json}
-import javax.inject._
-import models._
-import play.api.data.Form
-import play.api.data.Forms._
-import play.api.data.validation.Constraints._
-import play.api.i18n._
-import play.api.libs.json.Json
-import play.api.mvc._
 
-import scala.collection.mutable.ArrayBuffer
+import com.mohiva.play.silhouette.api.actions.SecuredRequest
+import com.mohiva.play.silhouette.api.{ LogoutEvent, Silhouette }
+import org.webjars.play.WebJarsUtil
+import play.api.i18n.I18nSupport
+import play.api.mvc.{ AbstractController, AnyContent, ControllerComponents }
+import utils.auth.DefaultEnv
 
-class ApplicationController @Inject()(
-                                       components: ControllerComponents
-                                     )(
-                                       implicit
-                                       webJarsUtil: WebJarsUtil,
-                                       assets: AssetsFinder,
-                                       ec: ExecutionContext
-                                     ) extends AbstractController(components) with I18nSupport {
+import scala.concurrent.Future
 
-  def index = Action.async { implicit request =>
-    Future.successful(Redirect("http://localhost:3000/"))
+/**
+  * The basic application controller.
+  *
+  * @param components  The Play controller components.
+  * @param silhouette  The Silhouette stack.
+  * @param webJarsUtil The webjar util.
+  * @param assets      The Play assets finder.
+  */
+class ApplicationController @Inject() (
+                                        components: ControllerComponents,
+                                        silhouette: Silhouette[DefaultEnv]
+                                      )(
+                                        implicit
+                                        webJarsUtil: WebJarsUtil,
+                                        assets: AssetsFinder
+                                      ) extends AbstractController(components) with I18nSupport {
+
+  /**
+    * Handles the index action.
+    *
+    * @return The result to display.
+    */
+  def index = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
+        Future.successful(Ok(views.html.home(request.identity)))
+//    Future.successful(Redirect("http://localhost:3000/"))
+  }
+
+  /**
+    * Handles the Sign Out action.
+    *
+    * @return The result to display.
+    */
+  def signOut = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
+    val result = Redirect(routes.ApplicationController.index())
+    silhouette.env.eventBus.publish(LogoutEvent(request.identity, request))
+    silhouette.env.authenticatorService.discard(request.authenticator, result)
   }
 }
-
--- build.sbt
+++ build.sbt
@@ -1,10 +1,14 @@
-name := """play-scala-slick-example"""
+name := "play-scala-slick-example"
 
-version := "2.6.x"
+version := "5.0.5"
+
+scalaVersion := "2.12.6"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
-scalaVersion := "2.12.6"
+routesImport += "utils.route.Binders._"
+
+TwirlKeys.templateImports := Seq()
 
 resolvers += Resolver.jcenterRepo
 
@@ -16,12 +20,25 @@ libraryDependencies += "com.typesafe.play" %% "play-slick" % "3.0.3"
 libraryDependencies += "com.typesafe.play" %% "play-slick-evolutions" % "3.0.3"
 libraryDependencies += "org.xerial"        %  "sqlite-jdbc" % "3.21.0"
 
-//libraryDependencies += "slick.driver.SQLiteDriver" %%
-
-//libraryDependencies += "com.h2database" % "h2" % "1.4.196"
-
-TwirlKeys.templateImports := Seq()
-libraryDependencies += specs2 % Test
-libraryDependencies ++= Seq("org.webjars" %% "webjars-play" % "2.6.3")
+libraryDependencies ++= Seq(
+  "com.mohiva" %% "play-silhouette" % "5.0.5",
+  "com.mohiva" %% "play-silhouette-password-bcrypt" % "5.0.5",
+  "com.mohiva" %% "play-silhouette-persistence" % "5.0.5",
+  "com.mohiva" %% "play-silhouette-crypto-jca" % "5.0.5",
+  "org.webjars" %% "webjars-play" % "2.6.3",
+  "org.webjars" % "bootstrap" % "3.3.7-1" exclude("org.webjars", "jquery"),
+  "org.webjars" % "jquery" % "3.2.1",
+  "net.codingwell" %% "scala-guice" % "4.1.0",
+  "com.iheart" %% "ficus" % "1.4.3",
+  "com.typesafe.play" %% "play-mailer" % "6.0.1",
+  "com.typesafe.play" %% "play-mailer-guice" % "6.0.1",
+  "com.enragedginger" %% "akka-quartz-scheduler" % "1.6.1-akka-2.5.x",
+  "com.adrianhurt" %% "play-bootstrap" % "1.4-P26-B3-SNAPSHOT",
+  "com.mohiva" %% "play-silhouette-testkit" % "5.0.5" % "test",
+  specs2 % Test,
+  ehcache,
+  guice,
+  filters
+)
 
 resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
